7942d94d743bd7c7b551a32dd719b5d86fd465f4
r-tamura <rtamura30@gmail.com>
2019-02-13 23:01:25 +0900

    Add ラムダ計算(INCREMENT/DECREMENT)

--- programming_with_nothing/lambda_calculus.test.ts
+++ programming_with_nothing/lambda_calculus.test.ts
@@ -1,52 +1,51 @@
 import test from "ava";
-import {
-  toInteger,
-  ZERO,
-  ONE,
-  TWO,
-  THREE,
-  FOUR,
-  FIVE,
-  toBoolean,
-  TRUE,
-  FALSE,
-  IF,
-  IS_ZERO,
-  PAIR,
-  LEFT,
-  RIGHT
-} from "./lambda_calculus";
+import * as L from "./lambda_calculus";
 
 test("toInteger", t => {
-  t.is(toInteger(ZERO), 0);
-  t.is(toInteger(THREE), 3);
+  t.is(L.toInteger(L.ZERO), 0);
+  t.is(L.toInteger(L.THREE), 3);
 });
 
 test("LambdaIntegers", t => {
-  t.is(toInteger(ONE), 1);
-  t.is(toInteger(TWO), 2);
-  t.is(toInteger(THREE), 3);
-  t.is(toInteger(FOUR), 4);
-  t.is(toInteger(FIVE), 5);
+  t.is(L.toInteger(L.ONE), 1);
+  t.is(L.toInteger(L.TWO), 2);
+  t.is(L.toInteger(L.THREE), 3);
+  t.is(L.toInteger(L.FOUR), 4);
+  t.is(L.toInteger(L.FIVE), 5);
+  t.is(L.toInteger(L.FIFTEEN), 15);
+  t.is(L.toInteger(L.HUNDRED), 100);
 });
 
 test("toBoolean/LambdaBoolean", t => {
-  t.true(toBoolean(TRUE));
-  t.false(toBoolean(FALSE));
+  t.true(L.toBoolean(L.TRUE));
+  t.false(L.toBoolean(L.FALSE));
 });
 
 test("LambdaIf", t => {
-  t.is(IF(TRUE)("happy")("sad"), "happy");
-  t.is(IF(FALSE)("happy")("sad"), "sad");
+  t.is(L.IF(L.TRUE)("happy")("sad"), "happy");
+  t.is(L.IF(L.FALSE)("happy")("sad"), "sad");
 });
 
 test("IS_ZERO", t => {
-  t.is(IS_ZERO(ZERO), TRUE);
-  t.is(IS_ZERO(ONE), FALSE);
+  t.is(L.IS_ZERO(L.ZERO), L.TRUE);
+  t.is(L.IS_ZERO(L.ONE), L.FALSE);
 });
 
 test("LEFT/RIGHT", t => {
-  const pair = PAIR(THREE)(FIVE);
-  t.is(toInteger(LEFT(pair)), 3);
-  t.is(toInteger(RIGHT(pair)), 5);
+  const pair = L.PAIR(L.THREE)(L.FIVE);
+  t.is(L.toInteger(L.LEFT(pair)), 3);
+  t.is(L.toInteger(L.RIGHT(pair)), 5);
+});
+
+test("INCREMENT", t => {
+  t.is(L.toInteger(L.INCREMENT(L.ZERO)), 1);
+  t.is(L.toInteger(L.INCREMENT(L.FOUR)), 5);
+});
+
+test("DECREMENT", t => {
+  t.is(L.toInteger(L.DECREMENT(L.ZERO)), 0);
+  t.is(L.toInteger(L.DECREMENT(L.FIVE)), 4);
+  t.is(L.toInteger(L.DECREMENT(L.FIFTEEN)), 14);
+  t.is(L.toInteger(L.DECREMENT(L.HUNDRED)), 99);
+  t.is(L.toInteger(L.DECREMENT(L.FIFTEEN)), 14);
 });
--- programming_with_nothing/lambda_calculus.ts
+++ programming_with_nothing/lambda_calculus.ts
@@ -12,6 +12,12 @@ export const TWO: LambdaInteger = p => x => p(p(x));
 export const THREE: LambdaInteger = p => x => p(p(p(x)));
 export const FOUR: LambdaInteger = p => x => p(p(p(p(x))));
 export const FIVE: LambdaInteger = p => x => p(p(p(p(p(x)))));
+export const FIFTEEN: LambdaInteger = p => x =>
+  p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(x)))))))))))))));
+export const HUNDRED: LambdaInteger = p => x =>
+  new Array(100).fill(p).reduce((acc, v) => p(acc), x);
+// `p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(x))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))`
+// HUNDREDは本来、上の式だが長すぎて、エディタでの表示が難しいため簡易実装
 // 必要な数の分だけ続く...
 
 // 真偽値
@@ -24,15 +30,26 @@ type LambdaIf = (b: LambdaBoolean) => LambdaBoolean;
 export const IF: LambdaIf = b => b;
 
 // 述語
-// ZEROは一度もpを呼び出さないことを利用する
+// Note: ZEROは一度もpを呼び出さないことを利用する
 export const IS_ZERO = (n: LambdaInteger) => n(_ => FALSE)(TRUE);
 
 // ペア
-type LambdaPair = <T>(x: T) => (y: T) => (f: Proc2<T>) => T;
+type LambdaPair<T> = (f: Proc2<T>) => T;
+type LambdaPairConstructor = <T>(x: T) => (y: T) => LambdaPair<T>;
 type LambdaPairIndex = <T>(p: (f: Proc2<T>) => T) => T;
-export const PAIR: LambdaPair = x => y => f => f(x)(y);
+export const PAIR: LambdaPairConstructor = x => y => f => f(x)(y);
 export const LEFT: LambdaPairIndex = p => p(x => y => x);
 export const RIGHT: LambdaPairIndex = p => p(x => y => y);
+
+// 数値演算
+type LambdaIncrement = <T>(n: LambdaInteger) => (p: Proc<T>) => (x: T) => T;
+export const INCREMENT: LambdaIncrement = n => p => x => p(n(p)(x));
+// Note: decrementはトリッキー。SLIDEでの引数を[-1, 0]とすることで、slideの呼び出し回数-1
+// という状況を作り出せる
+type LambdaSlide = (p: LambdaPair<LambdaInteger>) => LambdaPair<LambdaInteger>;
+type LambdaDecrement = (n: LambdaInteger) => LambdaInteger;
+export const SLIDE: LambdaSlide = p => PAIR(RIGHT(p))(INCREMENT(RIGHT(p)));
+export const DECREMENT: LambdaDecrement = n => LEFT(n(SLIDE)(PAIR(ZERO)(ZERO)));
 
 /**
  * Assert用
